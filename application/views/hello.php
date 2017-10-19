e47f7be06a5cad18db475dc6f8edc2dc2ca5d4c9
ULM Co <ULM@Takayuki-no-iMac.local>
2017-10-19 17:43:08 +0900

    /helloでページ遷移

---
+++ application/views/hello.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Hello</title>
+  </head>
+  <body>
+    <h1>Hello,CodeIgniter!</h1>
+  </body>
+</html>
